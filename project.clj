d5a6657f4bac58cd75f1c0ea631db755afa2df7f
Frederic Peschanski <not provided>
2017-12-13 22:52:31 +0100

    early def

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/fredokun/latte-sets.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [latte "0.6.1-SNAPSHOT"]
-                 [latte-sets "0.1.2-SNAPSHOT"]
-                 [latte-integers "0.5.8-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [latte "0.100.0-SNAPSHOT"]
+                 [latte-sets "0.5.0-SNAPSHOT"]
+                 [latte-integers "0.7.1-SNAPSHOT"]]
   :codox {:output-path "docs"
           :metadata {:doc/format :markdown}
           :namespaces [latte-finsets.core
                        ]}
-  :plugins [[lein-codox "0.10.2"]])
+  :plugins [[lein-codox "0.10.3"]])
--- src/latte_finsets/core.clj
+++ src/latte_finsets/core.clj
@@ -2,13 +2,36 @@
   "The formalization of finite sets.
 "
 
-  (:refer-clojure :exclude [and or not set])
+  (:refer-clojure :exclude [and or not set <= int])
 
   (:require [latte.core :as latte :refer [definition defthm defaxiom defnotation
-                                          forall lambda ==>
+                                          forall lambda defimplicit
                                           assume have pose proof lambda forall]]
             [latte.quant :as q :refer [exists]]
             [latte.prop :as p :refer [<=> and or not]]
-            [latte.equal :as eq :refer [equal]]))
+            [latte.equal :as eq :refer [equal]]
+
+            [latte-sets.rel :as rel :refer [rel]]
+            [latte-sets.pfun :as pfun :refer [pfun]]
+                       
+            [latte-integers.core :as int :refer [int]]
+            [latte-integers.nat :as nat :refer [nat]]
+            [latte-integers.ord :as ord :refer [<=]]))
+
+(definition range
+  "The range set from `m` to `n`."
+  [[m int] [n int]]
+  (lambda [k int]
+    (and (<= m k)
+         (<= k n))))
+
+(definition counted-def
+  "The set `s` is counted from 1 to `n`"
+  [[T :type] [s (set T)] [n int]]
+  (exists [cf (pfun cf s (range int/one n))]
+    (pfun/pbijective cf)))
+
+
+
 
 
