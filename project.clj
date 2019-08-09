8ab5287ce3187057ff989d3d5e67ef2a50bea270
Frederic Peschanski <not provided>
2019-08-09 11:19:14 +0200

    deps updated

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/fredokun/latte-sets.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [latte "0.100.0-SNAPSHOT"]
-                 [latte-sets "0.5.0-SNAPSHOT"]
-                 [latte-integers "0.7.1-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [latte "1.0b3-SNAPSHOT"]
+                 [latte-sets "1.0b3-SNAPSHOT"]
+                 [latte-integers "0.10.0-SNAPSHOT"]]
   :codox {:output-path "docs"
           :metadata {:doc/format :markdown}
           :namespaces [latte-finsets.core
                        ]}
-  :plugins [[lein-codox "0.10.3"]])
+  :plugins [[lein-codox "0.10.6"]])
